d1c24580dacda7926e42c8f4879f620eeaa23b66
Ahmed Alasow <a.alasow01@gmail.com>
2022-10-30 20:51:08 +0000

    Updated functionality of the updatedTimeSlots and initialize functions

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,11 +1,10 @@
-var currentDay = document.getElementById("#currentDay");
 var options = {
     startTime: 9,
-    endTime: 19
+    endTime: 17
 }
-function save(e){
-    var hour = $(e.target()).parent().parent().attr('data-hour');
-    var task = $(e.target()).parent().prev().children().val();
+function savedButton(e){
+    var hour = $(e.target).parent().parent().attr('data-hour');
+    var task = $(e.target).parent().prev().children().val();
 
     localStorage.setItem(hour, task);
     console.log("saved");
@@ -27,10 +26,40 @@ function updateTimeSlots(){
 }
 
 function generateTimeSlots(){
-    
+    for(var hour = options.startTime; hour <= options.endTime; hour++){
+        var savedItem = localStorage.getItem(hour);
+
+        var timeSlot = $('div').addClass('row time-block');
+        timeSlot.attr('data-hour', hour);
+
+        var hourSlot = $('div').addClass('col-sm-2 hour').text(moment(hour, 'h').format('h A'));
+        var description = $('div').addClass('col-sm-8 row');
+        var textArea = $('textarea').addClass('col-md-12 description');
+        textArea.val(savedItem); 
+
+        var saveContainer = $('div').addClass('saveBtn d-flex justify-content-center align-items-center col-sm-2');
+        saveContainer.on('click', savedButton);
+        var saveTask = $('<i>').addClass('fas fa-save');
+
+        timeSlot.append(hourSlot);
+        timeSlot.append(description);
+        description.append(textArea);
+        timeSlot.append(saveContainer);
+        saveContainer.append(saveTask);
+
+        $('.container').append(timeSlot);
+    }
 }
 
 function initialize(){
+
+    generateTimeSlots();
+
+    updateTimeSlots();
+
+    var currentDay = moment().format('dddd MMMM Do YYYY, hh:mm:ss a');
+    $('#currentDay').text(currentDay);
+
     setInterval(function(){
         updateTimeSlots();
     }, 10000);
